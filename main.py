c11cb17fe52e12ccf71b6f6e341fe08586ae20b2
Drew Helgerson <tah568@msstate.edu>
2023-10-25 11:10:18 -0500

    ch_10C

--- main.py
+++ main.py
@@ -1,43 +1,22 @@
-'''
-Write a split_check function that returns the amount that each diner must pay to cover the cost of the meal.
+'''Write a loop that prints each country's population in country_pop.
 
-The function has four parameters:
-
-bill: The amount of the bill.
-people: The number of diners to split the bill between.
-tax_percentage: The extra tax percentage to add to the bill.
-tip_percentage: The extra tip percentage to add to the bill.
-The tax or tip percentages are optional and may not be given when calling split_check. Use default parameter values of 0.15 (15%) for 
-tip_percentage, and 0.09 (9%) for tax_percentage. Assume that the tip is calculated from the amount of the bill before tax.
-
-Sample output with inputs: 25 2
-
-Cost per diner: $15.50
-Sample output with inputs: 100 2 0.075 0.21
-
-Cost per diner: $64.25
+Sample output with input:
+'China:1365830000,India:1247220000,United States:318463000,Indonesia:252164800':
+China has 1365830000 people.
+India has 1247220000 people.
+United States has 318463000 people.
+Indonesia has 252164800 people.
 '''
 
-# FIXME: Write the split_check function. HINT: Calculate the amount of tip and tax,
-# add to the bill total, then divide by the number of diners.
+user_input = input()
+entries = user_input.split(',')
+country_pop = {}
 
-def split_check(bill,people,tax_percentage=.09,tip_percentage=.15):
-    tip = bill * tip_percentage
-    tax = bill * tax_percentage
-    total = bill + tip + tax
-    25
-    return total / people
+for pair in entries:
+    split_pair = pair.split(':')
+    country_pop[split_pair[0]] = split_pair[1]
+    # country_pop is a dictionary, Ex: { 'Germany':'82790000', 'France':'67190000' }
 
-bill = float(input())
-people = int(input())
-
-# Cost per diner at the default tax and tip percentages
-print(f'Cost per diner: ${split_check(bill, people):.2f}')
-
-bill = float(input())
-people = int(input())
-new_tax_percentage = float(input())
-new_tip_percentage = float(input())
-
-# Cost per diner at different tax and tip percentages
-print(f'Cost per diner: ${split_check(bill, people, new_tax_percentage, new_tip_percentage):.2f}')
+for country in country_pop:
+    pop = country_pop[country]
+    print(f'{country} has {pop} people.')
