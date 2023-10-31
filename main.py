0a3abc0f25b124f306c7e56d8a339f8e4f5527bd
Drew Helgerson <tah568@msstate.edu>
2023-10-31 14:17:21 -0500

    more methods go brrrr

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ class Point():
     def __init__(self,x:float = None, y:float = None) -> None:
         self.x = x
         self.y = y
-        pass
+        return self.coords()
+    def __str__(self) -> str:
+        return f"{self.x}, {self.y}"
+    def coords(self) -> list:
+        return [self.x,self.y]
     
 p1 = Point()
 
-print(p1.x,p1.y)
+print(p1)
