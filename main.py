c280915337effbbdf3af93a37eaa1efe8d00f79e
Drew Helgerson <tah568@msstate.edu>
2023-10-31 14:12:32 -0500

    first commit for object-based programming

--- main.py
+++ main.py
@@ -1,22 +1,11 @@
-'''Write a loop that prints each country's population in country_pop.
+import turtle
 
-Sample output with input:
-'China:1365830000,India:1247220000,United States:318463000,Indonesia:252164800':
-China has 1365830000 people.
-India has 1247220000 people.
-United States has 318463000 people.
-Indonesia has 252164800 people.
-'''
+class Point():
+    def __init__(self,x:float = None, y:float = None) -> None:
+        self.x = x
+        self.y = y
+        pass
+    
+p1 = Point()
 
-user_input = input()
-entries = user_input.split(',')
-country_pop = {}
-
-for pair in entries:
-    split_pair = pair.split(':')
-    country_pop[split_pair[0]] = split_pair[1]
-    # country_pop is a dictionary, Ex: { 'Germany':'82790000', 'France':'67190000' }
-
-for country in country_pop:
-    pop = country_pop[country]
-    print(f'{country} has {pop} people.')
+print(p1.x,p1.y)
