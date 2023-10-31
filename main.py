aaadacf288f469e64ba8374e91da182fa74b13d0
Drew Helgerson <tah568@msstate.edu>
2023-10-31 14:26:02 -0500

    even mooooore methods

--- main.py
+++ main.py
@@ -4,11 +4,17 @@ class Point():
     def __init__(self,x:float = None, y:float = None) -> None:
         self.x = x
         self.y = y
-        return self.coords()
     def __str__(self) -> str:
         return f"{self.x}, {self.y}"
     def coords(self) -> list:
         return [self.x,self.y]
+    def setX(self,newx):
+        self.x = newx
+    def setY(self,newy):
+        self.y = newy
+    def getX(self): return self.x
+    def gety(self): return self.y
+    
     
 p1 = Point()
 
