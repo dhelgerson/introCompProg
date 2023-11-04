ba4b3ae285258e2448655e511ad26cbdcd065411
Drew Helgerson <tah568@msstate.edu>
2023-11-04 12:00:48 -0500

    final code, 100 recieved

--- babyNames.py
+++ babyNames.py
@@ -25,6 +25,18 @@ Decade  Rank
 
 import os,re
 
+def getRank(filename:str,name:str,gender:str) -> str:
+    gender = int(gender)
+    with open(filename) as file:
+        lines = file.read().split('\n')
+        for line in lines:
+            line = line.split('\t')
+            try:
+                if name == line[1 if gender == 1 else 3]:
+                    return line[0]
+            except:
+                return None
+
 def main():
     name = input('Enter the baby name you wish to research:  ').title()
     print('''          
@@ -32,44 +44,26 @@ Which gender list do you wish to research?
 1.  Male
 2.  Female'''
     )
-    gender = input('Enter Selection:  ')
+    gender = input('Enter selection:  ')
     while gender not in ['1','2']: gender = input('ERROR:  must enter a 1 or a 2\nPlease re-enter:  ')
     
     files = os.listdir()
-    files = [file if '19' in file else None for file in files]
+    files = [file if '19' in file else None for file in os.listdir()]
     while None in files: files.remove(None)
-    maleYears,femaleYears = {},{}
     files.sort()
-    for file in files:
-        with open(file) as file:
-            lines = file.read().split('\n')
-            yr = re.findall('([0-9]{4})',lines[0])[0]
-            maleYears[yr],femaleYears[yr] = {},{}
-            for line in lines:
-                line = line.split('\t')
-                try: rank,maleName,maleNum,femaleName,femaleNum = line[0],line[1],line[2],line[3],line[4]
-                except IndexError: continue
-                maleYears[yr][maleName] = {
-                    'rank': rank,
-                    'number':maleNum
-                }
-                femaleYears[yr][femaleName] = {
-                    'rank': rank,
-                    'number': femaleNum
-                }
     
     print(f'''
 For name:  {name}
 Decade  Rank
 ======  ==========''')
     
-    for year,names in (maleYears if gender == '1' else femaleYears).items():
-        try: rank = names[name]['rank']
-        except KeyError: print(f"{year}\tNot ranked");continue
-        print(f"{year}\t{rank}")
-    
-
-
+    for file in files:
+        rank,yr = getRank(file,name,gender),re.findall('([0-9]{4})',file)[0]
+        if rank == None:
+            print(f"{yr}\tNot ranked")
+        else:
+            print(f"{yr}\t{rank}")
+        
 
 if __name__ == '__main__':
     main()
