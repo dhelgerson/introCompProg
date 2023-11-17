3a13ddd6f2a3b0c5e645d157edd136525eea7e9a
Drew Helgerson <tah568@msstate.edu>
2023-11-17 10:06:45 -0600

    100

--- main.py
+++ main.py
@@ -1,59 +1,117 @@
-from __future__ import annotations
-import turtle
-class Point():
-    def __init__(self,x:float = None, y:float = None, color = 'green') -> None:
-        self.x = x
-        self.y = y
-        self.t = turtle.Turtle()
-        self.t.color(color)
-        self.t.goto(self.x,self.y)
-        self.t.shape('circle')
-        self.t.up()
-    def pos(self) -> tuple:
-        return self.x, self.y
-    def __str__(self) -> str: return f"{self.x}, {self.y}"
-    def __mul__(self,divisor):
-        return Point(self.x / divisor, self.y / divisor)
-    def coords(self) -> list: return [self.x,self.y]
-    def setX(self,newx): self.x = newx
-    def setY(self,newy): self.y = newy
-    def getX(self): return self.x
-    def gety(self): return self.y
-    def updateLocation(self):
-        self.t.goto(self.x,self.y)
-    def getMidpointBewteen(self, other:Point) -> Point:
-        """returns a Point who's location is the midpoint between self and other
+from random import randint
+from random import seed
+import time
 
-        Args:
-            other (Point): other point
+#Add Car class here
 
-        Returns:
-            Point: Midpoint
+class Car:
+    def __init__(self,driver:str,model:str='unknown',):    
+        """This method needs to accept the name of the driver (a string) and the model of the car (also a string).  
+        Use the string “unknown” for the default argument.  It should also initialize the number of hits that 
+        the car has received in the game to 0.
         """
-        return Point((self.x + other.y) / 2, (self.y + other.y) / 2)
+        self.driver,self.model = driver,model
+        self.hits = 0
     
-class Line:
-    def __init__(self, **args) -> None:
-        """draws a line using any 2 args
+    def __str__(self):    
+        """This method should return a string that consists of the name of the driver, followed by a tab, and then
+        the name of the model of the car."""
+        return f"{self.driver}\t{self.model}"
+
+    def get_driver(self):    
+        """This method simply returns the name of the driver."""
+        return self.driver
+
+    def get_model(self):    
+        """This method simply returns the model of the car."""
+        return self.model
+
+    def get_hits(self):    
+        """This method returns the number of times the car has been hit during the course of the derby."""
+        return self.hits
+
+    def add_hit(self):    
+        """This method adds 1 to the total number of hits."""
+        self.hits += 1
+
+
+
+#Do not change any code past this point
+def main():
+    num = int(input("Let's get started with a number between 0 and 10:  "))
+    seed(num)
+    #Registration from the file competitors.txt
+    cars = []
+
+    car_file = open("competitors.txt")
+
+    for each in car_file:
+        car, type_car = each.split('\t')
+        new_entry = Car(car.strip(), type_car.strip())
+        cars.append(new_entry)
         
-        Args:
-            slope (float): slope of the line
-            yInt (float): y-intercept of the line
-            p1 (Point): any point on the line
-            p2 (Point): any other point on the line
-        """
-        if len(args) < 2:
-            raise Exception("Must have 2 references for a line in 2d space")
-        for key,value in args.items():
-            '%s = %s' % (key,value)
-        print(p2)
+    print("There are now", len(cars), "cars in this here demolition derby.")    
+    print("Registration is now closed.  Here's who we have in our race:\n")
+    for each in cars:
+        print(each)
+
+    #Welcome
+    print("\nAnd now it's time for the race!\n")
+    print("Welcome ladies and gentlemen to the CSE 1284 Demolition Derby.")
+    print("We have cars in the arena to rock your world with all of the smashing")
+    print("and crashing that you could ever hope for.  It promises to be an")
+    print("exciting night.\n")
+
+    print("Our returning champion, " + cars[0].get_driver()+ ", has come tonight")
+    print("with his newest " + cars[0].get_model()+ ".")
+    print("The race hasn't started yet so he has " + str(cars[0].get_hits()) + " hits so far,")
+    print("but is he ever mobile.\nYee-haa!  Look at him move.  Yessiree, it should")
+    print("be a fine show.")
+
+
+    #Start of commented out section -- starting the derby  
+    print("Let's start this game.\n\nBANG!\n\nAnd we're off!\n\n")
+
+    while len(cars) > 1:    #Continue until one champ remains
+        if len(cars) < 5:
+            print("We're down to", len(cars), "cars in the race.")
+        else:
+            print("We still have", len(cars), "cars in the race.")
+
+        car_total = len(cars)
+        #Collisions
+        basher = randint(0, car_total - 1)
+        bashed = randint(0, car_total - 1)
+        #A car can't hit itself
+        while basher == bashed:
+            bashed = randint(0, car_total - 1)
+        
+        print(cars[basher].get_driver(), "just hit", cars[bashed].get_driver(), "and...")
+
+        hit_hard = randint(1, 10)
+        if hit_hard > 6: #Can no longer move
+            out = cars.pop(bashed)
+            print("Yes!  It looks like he can't get his ol' ", out.get_model(), "movin'.")
+            print("He's out of the game!")
             
-        
-        
+        else: #Add a hit to the car
+            cars[bashed].add_hit()
+            print("But he's still moving -- so he's still in the game.")
+        print("\n")
+        time.sleep(2)
+
+    #Ending announcements
+    print("The crowd is going WILD!  There's only one car left now.")
+    print("So with ", cars[0].get_hits(), "hits in this game, our new champion is.....")
+    print("drum-roll please.....\n")
+    print(cars[0].get_driver() + "!!!")
+    print("\nEverybody be sure to say a big CONGRATULATIONS to our champion.")
+    print("And be sure to come back next year, but until then, keep on crashin'")
+    print("and a-smashin'!")
+    print()
     
-if __name__ == '__main__':
-    p1 = Point(1,1)
-    p2 = Point(2,2)
-    line1 = Line(p1=p1,p2=p2)
-    
-    turtle.exitonclick()
+                   
+
+
+if __name__ == "__main__":
+    main()
